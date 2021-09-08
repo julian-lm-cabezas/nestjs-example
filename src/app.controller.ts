08e545a5754b6f4377b73c747b6293e9a757477f
julian-lm-cabezas <julian-harry.cabezas@ext.leroymerlin.es>
2021-09-08 11:57:27 +0200

    access different info from controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Req} from '@nestjs/common';
+import { Request } from 'express';
 
 //Generic HealthCheck
 @Controller()
 export class AppController {
   @Get('/health')
   getHealth(): string { return 'server up & running'; }
+
+  //access query params  [example => /queryParam?id=1&name=Peter]
+  @Get('/queryParams')
+  getQuery(@Query() query: any): any { return query; }
+
+  //access request/response headers
+  @Get('/headers')
+  getHeaders(@Req() req: Request): any { return {headers: req.headers} }
 }
