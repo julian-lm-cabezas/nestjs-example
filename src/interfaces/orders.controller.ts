fe383d2f24a88dab1e50e3a2c20bf0b3897d8336
julian-lm-cabezas <julian-harry.cabezas@ext.leroymerlin.es>
2021-09-10 13:56:33 +0200

    set swagger HTTP response

--- src/interfaces/orders.controller.ts
+++ src/interfaces/orders.controller.ts
@@ -1,9 +1,10 @@
-import {Controller, Get, Post, Put, Delete, Param, Body} from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger';
+import {Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, BadRequestException} from '@nestjs/common'
+import { ApiBody, ApiOkResponse, ApiTags, ApiBadRequestResponse, ApiNotFoundResponse, ApiInternalServerErrorResponse } from '@nestjs/swagger';
 import { OrderQueryService } from 'src/application/queryServices/order.query.service'
 import { OrderCommandService } from 'src/application/commandServices/order.command.service'
 import { Order } from 'src/domain/models/entities/order'
 import { LMESCommandController, LMESQueryController } from 'src/utils/LMESRestController';
+import { ObjectID } from 'mongodb';
 
 // use ApiTags to include controller calls under swagger Tag
 @ApiTags('orders')
@@ -13,18 +14,45 @@ export class OrderController implements LMESQueryController<Order>, LMESCommandC
     constructor(private queryService: OrderQueryService, private commandService: OrderCommandService) {}
     
     @Get()
+    @ApiOkResponse({ type: Order, isArray:true, description: 'Returns list of orders'})
+    @ApiInternalServerErrorResponse({ description: 'Error fetching orders.'})
     async findAll(): Promise<Order[]>{ return this.queryService.findAll() }
+    
+    
     @Get(':id')
-    async findOne(@Param('id') id: string): Promise<Order>{ return this.queryService.findById(id) }
+    @ApiOkResponse({type: Order, description: 'Returns order'})
+    @ApiBadRequestResponse({ description:'Bad Request'})
+    @ApiNotFoundResponse({ description: 'Order Not Found'})
+    @ApiInternalServerErrorResponse({ description: 'Error fetching orders.'})
+    async findOne(@Param('id') id: string): Promise<Order>{ 
+        if(!ObjectID.isValid(id)) throw new BadRequestException('Bad request')
+        const order = await this.queryService.findById(id) 
+        if(!order) throw new NotFoundException('Not found');
+        else return order
+    }
    
     findByCriteria(query: any): Promise<Order[]>{ return null }
 
     @Post()
+    @ApiOkResponse({type: Order, description: 'Returns order'})
+    @ApiBadRequestResponse({ description:'Bad Request'})
+    @ApiInternalServerErrorResponse({ description: 'Error fetching orders.'})
+    @ApiBody({ description: 'order',  type: Order })
     async insertOne(order: Order): Promise<Order>{ return this.commandService.insertOne(order) }
+
+
     @Put()
+    @ApiOkResponse({type: Order, description: 'Returns order'})
+    @ApiBadRequestResponse({ description:'Bad Request'})
+    @ApiInternalServerErrorResponse({ description: 'Error fetching orders.'})
+    @ApiBody({ description: 'order',  type: Order })
     updateOne(@Body() order: Order): Promise<Order>{ return this.commandService.updateOne(order) }
     
     @Delete()
+    @ApiOkResponse({type: Order, description: 'Returns order'})
+    @ApiBadRequestResponse({ description:'Bad Request'})
+    @ApiNotFoundResponse({ description: 'Order Not Found'})
+    @ApiInternalServerErrorResponse({ description: 'Error fetching orders.'})
     async deleteOne(@Param('id') id: string): Promise<boolean>{ 
         await this.commandService.deleteOne(id) 
         return true
--- src/utils/LMESEntity.ts
+++ src/utils/LMESEntity.ts
@@ -1,10 +1,12 @@
 import { ObjectIdColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
+import { ObjectID } from 'mongodb';
 
 export class LMESEntity{
     //add Api property to show attributes in Schema
     // defines property as ObjectId
     @ApiProperty()
     @ObjectIdColumn()
-    _id: string;
+    _id: ObjectID;
+;
 }
